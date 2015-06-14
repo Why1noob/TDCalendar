9333af85d83a44a100d2f63e4fb430e067aac7a2
Why1noob <Why1noob@github.com>
2015-06-14 23:33:50 +0400

    still fucking with js and timer button

--- TDCalendar/js/layoutControls.js
+++ TDCalendar/js/layoutControls.js
@@ -1,9 +1,12 @@
 ﻿function startStopTimer() {
     var timerButton = $("#timer-btn");
+    console.log(timerButton.text());
     if (timerButton.text() == "Запустить таймер") {
-        timerButton.text = "Остановить таймер";
+        timerButton.text("Остановить таймер");
+        setTimeout(function () { console.log("timer started") }, 1000);
     }
     if (timerButton.text() == "Остановить таймер") {
-        timerButton.text = "Запустить таймер";
+        timerButton.text("Запустить таймер");
+        setTimeout(function () { console.log("timer stoped") }, 1000);
     }
 }
